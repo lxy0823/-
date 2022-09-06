2605bb0214e769752acd0223a83079e0ed1c0852
1 <1103836770@qq.com>
2022-09-06 11:25:04 +0800

    fear 头条初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0, // 查看报错 0不提示 1warn 2 error
+    'vue/multi-word-component-names': 0 // 组件允许一个名字命名
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+const cpns = resolve(__dirname, 'src/components')
+const views = resolve(__dirname, 'src/views')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 8888,
+      host: 'localhost'
+    },
+    resolve: {
+      alias: {
+        cpns,
+        views
+      }
+    }
+  }
 })
